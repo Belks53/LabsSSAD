2b5ab89cdf40d74831bf6ba8f46c4c8578f3d1ea
Belks53 <sigalbella35@gmail.com>
2026-02-17 18:06:43 +0300

    task1 cont.

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <utility>
 
 template <typename T>
 class GenericStack{
@@ -20,11 +21,11 @@ private:
     top = -1;
   }
 
-  void push(T x) {
+  virtual void push(T x) {
     if (top == capacity - 1) {
       resize();
     }
-    data[++top] = x;
+    data[++top] = std::move(x);
   }
 
   T pop(){
@@ -38,11 +39,15 @@ private:
     return result;
   }
 
-  T peek() const {
+  [[nodiscard]] T peek() const {
     if (top == -1) {
       throw std::out_of_range("Stack is empty");
     }
     return data[top];
+  }
+
+  [[nodiscard]] int size() const {
+    return top+1;
   }
 
 private:
@@ -59,15 +64,46 @@ private:
   }
 };
 
+class StringStack: public GenericStack<std::string> {
+public:
+  explicit StringStack(int cap) : GenericStack<std::string>(cap) {}
+
+  void push(const std::string x) override {
+    if (x.empty()) {
+      throw std::invalid_argument("String is empty");
+    }
+    GenericStack<std::string>::push(x);
+  }
+
+  void concatTopTwo() {
+    if (this->size() < 2) {
+      throw std::out_of_range("Not enough elements to concat");
+    }
+    std::string s1 = this->pop();
+    std::string s2 = this->pop();
+    std::string s3 = s1 + s2;
+    this->push(s3);
+  }
+};
+
 int main() {
-  GenericStack<int> a = GenericStack<int>(10);
-  a.push(10);
-  a.push(20);
-  a.push(30);
-  std::cout << a.peek() << std::endl;
-  std::cout << a.pop() << std::endl;
-  std::cout << a.peek() << std::endl;
+  try {
+    GenericStack<int> a = GenericStack<int>(10);
+    a.push(10);
+    a.push(20);
+    a.push(30);
+    std::cout << a.peek() << std::endl;
+    std::cout << a.pop() << std::endl;
+    std::cout << a.peek() << std::endl;
 
+    StringStack s = StringStack(10);
+    s.push("bbbb");
+    s.concatTopTwo();
 
+    std::cout << s.peek() << std::endl;
+  } catch (std::exception& e) {
+    std::cout << e.what() << std::endl;
+  }
 
+  return 0;
 }
