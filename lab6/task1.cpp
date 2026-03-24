08d92a71285870fbbd7c504aa5eed33813ec2c08
Belks53 <sigalbella35@gmail.com>
2026-03-24 16:41:34 +0300

    add lab9 task1.cpp

--- lab6/task1.cpp
+++ lab6/task1.cpp
@@ -1,34 +1,34 @@
-#include <cstring>
-#include <iostream>
-#include <ostream>
-
-template <typename T>
-class Wrapper {
-    private:
-    T value;
-
-    public:
-    explicit Wrapper(T value) : value(value) {}
-
-    T getValue() {
-      return value;
-    }
-};
-
-template <>
-class Wrapper<const char*> {
-    private:
-    const char* value;
-
-    public:
-    explicit Wrapper(const char* value) : value(value) {}
-
-    int getValue() {
-        return strlen(value);
-    }
-};
-
-int main() {
-    Wrapper<const char*> b("hello");
-    std::cout << b.getValue() << std::endl;
-}
+// #include <cstring>
+// #include <iostream>
+// #include <ostream>
+//
+// template <typename T>
+// class Wrapper {
+// private:
+//     T value;
+//
+// public:
+//     explicit Wrapper(T value) : value(value) {}
+//
+//     T getValue() {
+//         return value;
+//     }
+// };
+//
+// template <>
+// class Wrapper<const char*> {
+// private:
+//     const char* value;
+//
+// public:
+//     explicit Wrapper(const char* value) : value(value) {}
+//
+//     int getValue() {
+//         return strlen(value);
+//     }
+// };
+//
+// int main() {
+//     Wrapper<const char*> b("hello");
+//     std::cout << b.getValue() << std::endl;
+// }
--- lab6/task2.cpp
+++ lab6/task2.cpp
@@ -1,83 +1,83 @@
-#include <map>
-#include <iostream>
-#include <string>
-
-template <typename K, typename V>
-class Dictionary {
-    private:
-    std::pmr::map<K, V> p;
-
-    public:
-    explicit Dictionary() = default;
-
-
-    V get(K key) {
-        auto it = p.find(key);
-        if (it == p.end()) {
-            return V();
-        }
-        return it->second;
-    }
-
-    void put(K key, V value) {
-        p[key] = value;
-    }
-
-    void remove(K key) {
-        p.erase(key);
-    }
-
-    int size() {
-        return p.size();
-    }
-};
-
-template <typename K>
-class Dictionary<K, int> {
-private:
-    std::pmr::map<K, int> p;
-
-public:
-    explicit Dictionary() = default;
-
-    int get(K key) {
-        return abs(p.find(key)->second);
-    }
-
-    void put(K key, int value) {
-        p[key] = value;
-    }
-
-    void remove(K key) {
-        p.erase(key);
-    }
-
-    int size() {
-        int result = 0;
-        for (auto it = p.begin(); it != p.end(); it++) {
-            result += it->second;
-        }
-        return result;
-    }
-
-};
-
-int main() {
-    // Dictionary<int, std::string> a;
-    // a.put(1, "hello");
-    // a.put(2, "world");
-    // a.size();
-    // std::cout << a.get(1) << std::endl;
-    // a.remove(1);
-    // a.size();
-    // std::cout << a.get(2) << std::endl;
-
-    Dictionary<std::string, int> dic;
-
-    dic.put("hello", -1);
-    dic.put("world", 2);
-    dic.put("eded", 3);
-
-    std::cout << dic.get("hello") << std::endl;
-    std::cout << dic.size();
-}
+// #include <map>
+// #include <iostream>
+// #include <string>
+//
+// template <typename K, typename V>
+// class Dictionary {
+//     private:
+//     std::pmr::map<K, V> p;
+//
+//     public:
+//     explicit Dictionary() = default;
+//
+//
+//     V get(K key) {
+//         auto it = p.find(key);
+//         if (it == p.end()) {
+//             return V();
+//         }
+//         return it->second;
+//     }
+//
+//     void put(K key, V value) {
+//         p[key] = value;
+//     }
+//
+//     void remove(K key) {
+//         p.erase(key);
+//     }
+//
+//     int size() {
+//         return p.size();
+//     }
+// };
+//
+// template <typename K>
+// class Dictionary<K, int> {
+// private:
+//     std::pmr::map<K, int> p;
+//
+// public:
+//     explicit Dictionary() = default;
+//
+//     int get(K key) {
+//         return abs(p.find(key)->second);
+//     }
+//
+//     void put(K key, int value) {
+//         p[key] = value;
+//     }
+//
+//     void remove(K key) {
+//         p.erase(key);
+//     }
+//
+//     int size() {
+//         int result = 0;
+//         for (auto it = p.begin(); it != p.end(); it++) {
+//             result += it->second;
+//         }
+//         return result;
+//     }
+//
+// };
+//
+// int main() {
+//     // Dictionary<int, std::string> a;
+//     // a.put(1, "hello");
+//     // a.put(2, "world");
+//     // a.size();
+//     // std::cout << a.get(1) << std::endl;
+//     // a.remove(1);
+//     // a.size();
+//     // std::cout << a.get(2) << std::endl;
+//
+//     Dictionary<std::string, int> dic;
+//
+//     dic.put("hello", -1);
+//     dic.put("world", 2);
+//     dic.put("eded", 3);
+//
+//     std::cout << dic.get("hello") << std::endl;
+//     std::cout << dic.size();
+// }
