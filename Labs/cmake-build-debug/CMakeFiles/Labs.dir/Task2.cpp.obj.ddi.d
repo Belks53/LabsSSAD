a7a86a64e38a237ede9f239827a88a23adbde1ce
Belks53 <sigalbella35@gmail.com>
2026-04-07 16:36:52 +0300

    Delete Labs/cmake-build-debug directory

--- Labs/cmake-build-debug/CMakeFiles/Labs.dir/Task2.cpp.obj.ddi.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles\Labs.dir\Task2.cpp.obj.ddi: \
- C:/Users/Belka1/CLionProjects/Labs/Task2.cpp
--- Labs/cmake-build-debug/CMakeFiles/Labs.dir/task1.cpp.obj.ddi.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles\Labs.dir\task1.cpp.obj.ddi: \
- C:/Users/Belka1/CLionProjects/Labs/task1.cpp
