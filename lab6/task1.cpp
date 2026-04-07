cc5212ec62cbfc689ca8b6456796254535897d6d
Belks53 <sigalbella35@gmail.com>
2026-04-07 16:43:52 +0300

    Delate comments

--- lab6/task1.cpp
+++ lab6/task1.cpp
@@ -1,32 +1,32 @@
-// #include <cstring>
-// #include <iostream>
-// #include <ostream>
-//
-// template <typename T>
-// class Wrapper {
-// private:
-//     T value;
-//
-// public:
-//     explicit Wrapper(T value) : value(value) {}
-//
-//     T getValue() {
-//         return value;
-//     }
-// };
-//
-// template <>
-// class Wrapper<const char*> {
-// private:
-//     const char* value;
-//
-// public:
-//     explicit Wrapper(const char* value) : value(value) {}
-//
-//     int getValue() {
-//         return strlen(value);
-//     }
-// };
+#include <cstring>
+#include <iostream>
+#include <ostream>
+
+template <typename T>
+class Wrapper {
+private:
+    T value;
+
+public:
+    explicit Wrapper(T value) : value(value) {}
+
+    T getValue() {
+        return value;
+    }
+};
+
+template <>
+class Wrapper<const char*> {
+private:
+    const char* value;
+
+public:
+    explicit Wrapper(const char* value) : value(value) {}
+
+    int getValue() {
+        return strlen(value);
+    }
+};
 //
 // int main() {
 //     Wrapper<const char*> b("hello");
